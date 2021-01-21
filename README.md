8b4bb6bc0d879b9f9618ea402e3a155b1aa57476
Rishikesh46 <rishikesh.raut@raweng.com>
2021-01-21 12:08:53 +0530

    changes

--- README.md
+++
@@ -1,4 +0,0 @@
-Created a simple express server application that serves blog data. The api end-points supports the following operations
-
-   - Hosted on heroku.
-https://raw-data.herokuapp.com/blogs/surfboard
--- helpers/sendError.js
+++
@@ -1,9 +0,0 @@
-const sendErrorMessgae = (error, req, res) => {
-    res.status(error.statusCode).json({
-      status: error.status,
-      message: error.message,
-    });
-  };
-  
-  module.exports = sendErrorMessgae;
-  
